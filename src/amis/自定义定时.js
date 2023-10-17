65348705e39ba746070ac2e8ba1d94937b26ccc8
nbn <nabaonan@yunlizhihui.com>
2023-10-17 22:59:10 +0800

    fix: 完善

--- src/amis/自定义定时.js
+++ src/amis/自定义定时.js
@@ -2,7 +2,7 @@
  * @Author: nbn
  * @Date: 2023-10-17 22:01:11
  * @LastEditors: nbn
- * @LastEditTime: 2023-10-17 22:46:34
+ * @LastEditTime: 2023-10-17 22:58:25
  * @FilePath: /amis-widget/src/amis/自定义定时.js
  * @Description: 
  */
@@ -21,7 +21,7 @@ export default {
           "engine": "Trident - tagmei",
           "browser": "Internet Explorer 4.0",
           "platform": "Win 95+",
-          "count": "2023-10-17 23:44:13",
+          "count": 1697554800000,
           "grade": "X",
           "badgeText": "默认",
           "id": 1
@@ -38,56 +38,47 @@ export default {
           "label": "倒计时",
           "type": "custom",
           "onMount": (dom, value, onChange) => {
-
-
-
-
-
-
-
             const getTime = (dateString) => {
               var now = new Date();
-
               var target = new Date(dateString);
-
               var difference = target.getTime() - now.getTime();
-
               var day = parseInt(difference / (1000 * 60 * 60 * 24));
-
-
-
               var hour = parseInt((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 
               var minute = parseInt((difference % (1000 * 60 * 60)) / (1000 * 60));
-
               var second = parseInt(difference % (1000 * 60) / 1000);
-
               var ms = difference % 1000;
+              if (ms <= 0) {
+                ms = 0
+              }
+              if (second <= 0) {
+                second = 0
+              }
+              if (minute <= 0) {
+                minute = 0
+              }
+              if (hour <= 0) {
+                hour = 0
+              }
 
               return [
-
                 day,
-
                 hour,
-
                 minute,
-
                 second,
-
                 ms
-
               ];
 
             }
-
-
-
-
-            dom.inter = setInterval(() => {
+            const inter = setInterval(() => {
               const [day, hour, minute, ms] = getTime(value)
+              if (ms == 0 && minute == 0 && hour == 0 && day == 0) {
+                clearInterval(inter)
+              }
               dom.innerText = `${day}天${hour}时${minute}分${ms}秒`
-              // onChange(count)  
             }, 1000)
+
+
           },
 
           "onUpdate": (dom, data, prevData) => {
